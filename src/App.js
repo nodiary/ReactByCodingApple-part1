5ed703c69b09cc8422aa05dfccf0117f543bac20
nodiary <rohilkie94@gmail.com>
2024-01-09 09:43:12 +0900

    4강 state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [글제목, b] = useState(['남자 코트 추천', '여자 코트 추천', '아동 코트 추천']);
+
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={ {color: 'red', fontSize : '16px'} }>블로그임</h4>
+        <h4 style={ {color: 'red', fontSize : '16px'} }>ReactBlog</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className="list">
+        <h4>{ 글제목[0] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
